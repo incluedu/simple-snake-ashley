8d61d7a71f0a0fa3a7b377212924d0898b1cd421
Patric Hollenstein <patric.hollenstein@lustenauer.net>
2018-02-13 19:40:10 +0100

    after session 187

--- core/src/net/lustenauer/snake/screen/game/GameScreen.java
+++ core/src/net/lustenauer/snake/screen/game/GameScreen.java
@@ -1,7 +1,16 @@
 package net.lustenauer.snake.screen.game;
 
+import com.badlogic.ashley.core.PooledEngine;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import net.lustenauer.snake.SimpleSnakeGame;
+import net.lustenauer.snake.common.GameManager;
+import net.lustenauer.snake.config.GameConfig;
+import net.lustenauer.snake.screen.menu.MenuScreen;
+import net.lustenauer.snake.system.GridRenderSystem;
 import net.lustenauer.snake.util.GdxUtils;
 
 /**
@@ -15,13 +24,18 @@ public class GameScreen extends ScreenAdapter{
      */
 
     private final SimpleSnakeGame game;
+    private final AssetManager assetManager;
 
+    private Viewport viewport;
+    private ShapeRenderer renderer;
+    private PooledEngine engine;
 
     /*
      * CONSTRUCTORS
      */
     public GameScreen(SimpleSnakeGame game) {
         this.game = game;
+        assetManager = game.getAssetManager();
     }
 
     /*
@@ -29,11 +43,38 @@ public class GameScreen extends ScreenAdapter{
      */
 
     @Override
-    public void render(float delta) {
-        GdxUtils.clearScreen();
+    public void show() {
+        viewport = new FitViewport(GameConfig.WORLD_WIDTH, GameConfig.WORLD_HEIGHT);
+        renderer = new ShapeRenderer();
+        engine = new PooledEngine();
+
+        engine.addSystem(new GridRenderSystem(viewport,renderer));
     }
 
+    @Override
+    public void render(float delta) {
+        GdxUtils.clearScreen();
 
+        engine.update(delta);
 
+        if (GameManager.INSTANCE.isGameOver()) {
+            game.setScreen(new MenuScreen(game));
+        }
+    }
 
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width,height,true);
+    }
+
+    @Override
+    public void hide() {
+        dispose();
+    }
+
+    @Override
+    public void dispose() {
+        renderer.dispose();
+        engine.removeAllEntities();
+    }
 }
---
+++ core/src/net/lustenauer/snake/system/GridRenderSystem.java
@@ -0,0 +1,37 @@
+package net.lustenauer.snake.system;
+
+import com.badlogic.ashley.core.EntitySystem;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import net.lustenauer.snake.util.ViewportUtils;
+
+/**
+ * Created by Patric Hollenstein on 13.02.18.
+ *
+ * @author Patric Hollenstein
+ */
+public class GridRenderSystem extends EntitySystem{
+
+    /*
+     * ATTRIBUTES
+     */
+    private final Viewport viewport;
+    private final ShapeRenderer renderer;
+
+    /*
+     * CONSTRUCTORS
+     */
+    public GridRenderSystem(Viewport viewport, ShapeRenderer renderer) {
+        this.viewport = viewport;
+        this.renderer = renderer;
+    }
+
+    /*
+     * UPDATE
+     */
+    @Override
+    public void update(float deltaTime) {
+        viewport.apply();
+        ViewportUtils.drawGrid(viewport,renderer);
+    }
+}
